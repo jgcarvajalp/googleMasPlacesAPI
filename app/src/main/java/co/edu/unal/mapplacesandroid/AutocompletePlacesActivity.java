48ef08a0852ace62940ba2f5c643180e88e8443a
Juan Carvajal <cpj3305@gmail.com>
2019-11-26 23:36:26 -0500

    Google maps with places API integration

--- app/src/main/java/co/edu/unal/mapplacesandroid/AutocompletePlacesActivity.java
+++ app/src/main/java/co/edu/unal/mapplacesandroid/AutocompletePlacesActivity.java
@@ -1,31 +1,41 @@
 package co.edu.unal.mapplacesandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.graphics.Color;
+import android.location.Location;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.compat.Place;
 import com.google.android.libraries.places.compat.ui.PlaceAutocompleteFragment;;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.libraries.places.compat.ui.PlaceSelectionListener;
 
 import java.io.UnsupportedEncodingException;
@@ -40,6 +50,8 @@ public class AutocompletePlacesActivity extends AppCompatActivity implements OnM
     static final int DIALOG_QUIT_ID = 1;
     private String mRadio = "1";
     private SharedPreferences mPrefs;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    View mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +62,7 @@ public class AutocompletePlacesActivity extends AppCompatActivity implements OnM
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (MapFragment) getFragmentManager()
                 .findFragmentById(R.id.map);
+        mapView = mapFragment.getView();
         setupAutoCompleteFragment();
 
     }
@@ -124,7 +137,7 @@ public class AutocompletePlacesActivity extends AppCompatActivity implements OnM
             public void onPlaceSelected(Place place) {
                 sydney = place.getLatLng();
                 mapFragment.getMapAsync(AutocompletePlacesActivity.this);
-                Toast.makeText(AutocompletePlacesActivity.this, "******** sydney" + sydney.latitude, Toast.LENGTH_SHORT).show();
+             //   Toast.makeText(AutocompletePlacesActivity.this, "******** sydney" + sydney.latitude, Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -139,7 +152,13 @@ public class AutocompletePlacesActivity extends AppCompatActivity implements OnM
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 8.5f));
-        mMap.setMyLocationEnabled(true);
+        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setOnMyLocationButtonClickListener(onMyLocationButtonClickListener);
+        mMap.setOnMyLocationClickListener(onMyLocationClickListener);
+        enableMyLocationIfPermitted();
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
        /* mMap.addMarker(new MarkerOptions()
                 .position(sydney)
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
@@ -169,6 +188,88 @@ public class AutocompletePlacesActivity extends AppCompatActivity implements OnM
         GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
 
         getNearbyPlacesData.execute(dataTransfer);
+
+        if (mapView != null &&
+                mapView.findViewById(Integer.parseInt("1")) != null) {
+            // Get the button view
+            View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+            // and next place it, on bottom right (as Google Maps app)
+            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams)
+                    locationButton.getLayoutParams();
+            // position on right bottom
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+            layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+            layoutParams.setMargins(0, 0, 30, 30);
+        }
+    }
+
+
+    private GoogleMap.OnMyLocationButtonClickListener onMyLocationButtonClickListener =
+            new GoogleMap.OnMyLocationButtonClickListener() {
+                @Override
+                public boolean onMyLocationButtonClick() {
+                    //mMap.setMinZoomPreference(15);
+
+
+                    return false;
+                }
+            };
+
+    private GoogleMap.OnMyLocationClickListener onMyLocationClickListener =
+            new GoogleMap.OnMyLocationClickListener() {
+                @Override
+                public void onMyLocationClick(@NonNull Location location) {
+
+                    //mMap.setMinZoomPreference(15);
+
+                    /*CircleOptions circleOptions = new CircleOptions();
+                    circleOptions.center(new LatLng(location.getLatitude(),
+                            location.getLongitude()));
+
+                    circleOptions.radius(200);
+                    circleOptions.fillColor(Color.RED);
+                    circleOptions.strokeWidth(6);
+
+                    mMap.addCircle(circleOptions);*/
+
+                    LatLng currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
+
+                    String types = "food|hospital|airport|library|liquor_store|pharmacy|restaurant|shopping_mall|university|gas_station";
+
+                    String url = null;
+                    try {
+                        url = getUrl(currentLoc.latitude, sydney.longitude, URLEncoder.encode(types,"UTF-8"));
+                    } catch (UnsupportedEncodingException e) {
+                        e.printStackTrace();
+                    }
+
+                    System.out.println("******************* URL: " + url);
+
+                    mMap.clear();
+
+                    Object dataTransfer[] = new Object[2];
+                    dataTransfer[0] = mMap;
+                    dataTransfer[1] = url;
+
+                    GetNearbyPlacesData getNearbyPlacesData = new GetNearbyPlacesData();
+
+                    getNearbyPlacesData.execute(dataTransfer);
+                }
+            };
+
+
+
+    private void enableMyLocationIfPermitted() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        } else if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
     }
 
     private String getUrl(double latitude , double longitude , String nearbyPlace)
@@ -176,10 +277,10 @@ public class AutocompletePlacesActivity extends AppCompatActivity implements OnM
 
         StringBuilder googlePlaceUrl = new StringBuilder("https://maps.googleapis.com/maps/api/place/nearbysearch/json?");
         googlePlaceUrl.append("location="+latitude+","+longitude);
-        googlePlaceUrl.append("&radius="+ mRadio);
+        googlePlaceUrl.append("&radius="+ (Integer.parseInt(mRadio) * 1000));
         googlePlaceUrl.append("&type="+nearbyPlace);
         googlePlaceUrl.append("&sensor=true");
-        googlePlaceUrl.append("&key="+"");
+        googlePlaceUrl.append("&key="+ getString(R.string.google_maps_key));
 
         Log.d("AutocompletePlacesActivity", "url = "+googlePlaceUrl.toString());
 
